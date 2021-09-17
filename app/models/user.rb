0cfb9eea9fa87d88290c28d1dc6d098f2102b14a
Melissa Flynn <mflynn814@gmail.com>
2021-09-17 11:34:13 -0700

    first commit -user auth

---
+++ app/models/user.rb
@@ -0,0 +1,58 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_session_token  (session_token)
+#  index_users_on_username       (username) UNIQUE
+#
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+
+  before_validation :ensure_session_token
+
+  attr_reader :password
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+
+end
