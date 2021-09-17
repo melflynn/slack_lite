0191357196a6a5c7c0f79fd5122f7401e7944bf4
Melissa Flynn <mflynn814@gmail.com>
2021-09-17 11:57:45 -0700

    Setup frontend

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if user
+      login(user)
+    else
+      render json: user, status: 422
+    end
+  end
+
+  def destroy
+    user = current_user
+    if user
+      logout
+    else
+      render json: ['Not currently logged in'], status: 404
+    end
+  end
+
+end
